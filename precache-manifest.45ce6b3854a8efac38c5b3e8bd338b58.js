bb9131fa01185254cc5bfe568cfff09b8c528c64
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-01 17:54:24 +0700

    Updates

---
+++ precache-manifest.45ce6b3854a8efac38c5b3e8bd338b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8c62c0b0104f7385a9dc07e8683ff7",
+    "url": "/thtop/react-robofriend-project/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/thtop/react-robofriend-project/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6a0f4d6e818f14f16f95",
+    "url": "/thtop/react-robofriend-project/static/css/main.9d60d544.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/thtop/react-robofriend-project/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "6a0f4d6e818f14f16f95",
+    "url": "/thtop/react-robofriend-project/static/js/main.5f7d242a.chunk.js"
+  },
+  {
+    "revision": "63a5ac2e51b1d814190b",
+    "url": "/thtop/react-robofriend-project/static/js/runtime-main.0a29bcf3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/thtop/react-robofriend-project/static/media/SEGA.12a3db8b.woff"
+  }
+]);
